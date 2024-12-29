88a8286fab512af528ec8f3c14a43c6481f85d35
my project <rahulmohabiya641@gmail.com>
2024-12-28 18:44:05 -0800

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,73 @@
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.mainDiv}>
+      <h1 className={styles.h1a}>Minimum Viable Product Development</h1>
+      <h5>MVP (minimum Viable Product) development a strategy to and lunch a simplified version of a product </h5>
+     <h5>with core features to test its viablity in the market</h5> <br></br> 
+      
+      <btn className={styles.btn}>  <li className={styles.font}>Get Your Free Estimated Now</li></btn>
+<br></br>
+<br></br>
+<br></br>
+      <h1 className={styles.h1}> Why Startups Choose Carolinas</h1>
+      <br></br><br></br>
+      <div className={styles.topDiv}>
+        
+        <div className={styles.card1}>
+          <h3 className={styles.h3}>
+          
+            <br></br>Cost savings</h3>
+          <br></br>
+
+          <h5 className= {styles.h5}>At Carolinas, we offer an onshore/offshore model that ensures <br></br> 
+          you'll work directly with onshore projects management while<br></br>
+          production is handied by offshore produsction teams</h5>
+          <br></br>
+          
+        </div>
+        
+        <div className={styles.card2}>
+          <h3  className={styles.h3a}>Access to Specialized Telent</h3>
+          <br></br>
+        <h5>Carlinas offers access to highly speciallized talent may be <br></br>defficult for startups to fin on their own</h5>
+        <h5></h5>
+        </div>
+      </div>
+      <div>
+        <br></br>
+      <h1 className={styles.h1}>Build Your team with expert</h1>
+      <br></br>
+      <br></br>
+
+      </div>
+
+      <div className={styles.bottomDiv}>
+        <div className={styles.card3a}>
+          <br></br>
+          <h4>Outsource your non-core activities to us,<br></br>so you can stay focused on growing your<br></br> 
+          business. Our dedicated team members <br></br> seemlessly intergrate with your business <br></br> 
+          so you can scale with confidence,</h4>
+          <br></br>
+          <btn className={styles.startbtn}>
+            <li className={styles.li}>Start Building Your Team</li></btn>
+
+        </div>
+
+        <div className={styles.card3}><h4>__________________________________________________________________<br></br>✔Credentialed: 100% of your staff will have a high <br></br>
+        school degree at a minimum: 70% of staff will have <br></br>a master's degree.<br></br><br></br>
+        __________________________________________________________________<br></br>
+✔Skilled: We require at least 1 years experience. Each <br></br>
+candidate is vetted using a series of screening <br></br>tests with a focus on data review proficiencies.<br></br>
+<br></br>__________________________________________________________________<br></br>
+✔ Dedicated: We have some of the lowest attrition <br></br>rates in the business. The average tenure of our<br>
+</br> technicians in 5 years.<br></br>
+<br></br>__________________________________________________________________<br></br>
+
+Scalable team: Grow your team from 1 to 30 in just</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
